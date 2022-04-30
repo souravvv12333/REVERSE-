0d704c89c035cda6cb41ed504597850a7c15ffc3
souravvv12333 <104692607+souravvv12333@users.noreply.github.com>
2022-04-30 23:59:15 +0530

    Add files via upload

---
+++ REVERSE.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+#include <string.h>  
+int main()  
+{  
+    char str[40]; // declare the size of character string  
+    printf (" \n MY captain ");  
+    scanf ("%s", str);  
+      
+    // use strrev() function to reverse a string  
+    printf (" \n NIATPAC YM %s ", strrev(str));  
+    return 0;  
+}  
+
+
+
+
